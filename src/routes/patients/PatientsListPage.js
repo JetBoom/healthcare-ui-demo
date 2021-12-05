6c63c491739041f9eb6f8ee94791d17935bf0d0f
William Moodhe <williammoodhe@gmail.com>
2021-12-05 18:16:35 -0500

    Use themed data table for patients. Add link to individual page + avatar

--- src/routes/patients/PatientsListPage.js
+++ src/routes/patients/PatientsListPage.js
@@ -1,38 +1,15 @@
-import { useState, useEffect } from 'react'
-import DataTable from 'react-data-table-component'
+import { useState, useEffect, useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Avatar, IconButton } from '@material-ui/core'
+import LaunchIcon from '@material-ui/icons/Launch'
+import DataTable from 'components/DataTable'
 import Page from 'components/Page'
 import Loading from 'components/Loading'
 import { GET } from 'util/dev'
 
 
-const columns = [
-  {
-    name: 'Name',
-    selector: (row) => row.name,
-    sortable: true,
-    grow: 2,
-  },
-  {
-    name: 'Age',
-    selector: (row) => row.age,
-    sortable: true,
-  },
-  {
-    name: 'Sex',
-    selector: (row) => row.sex,
-    sortable: true,
-    right: true,
-  },
-  {
-    name: 'Treatment Costs',
-    selector: (row) => row.treatmentCosts,
-    format: (row) => row.treatmentCosts.toLocaleString('en-US', {style: 'currency', currency: 'USD'}),
-    sortable: true,
-    right: true,
-  },
-]
-
 export default function PatientsListPage() {
+  const navigate = useNavigate()
   const [loading, setLoading] = useState(false)
   const [patients, setPatients] = useState([])
 
@@ -54,6 +31,42 @@ export default function PatientsListPage() {
 
     fetchData()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const columns = useMemo(() => ([
+    {
+      name: 'Name',
+      selector: (row) => row.name,
+      format: (row, patientId) => (
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <Avatar alt="" src={row.avatarUrl} style={{marginRight: '1ch'}} />
+          <span>{row.name}</span>
+          <IconButton size="small" onClick={() => navigate(`/patients/${patientId}`)}>
+            <LaunchIcon fontSize="small" />
+          </IconButton>
+        </div>
+      ),
+      sortable: true,
+      grow: 2,
+    },
+    {
+      name: 'Age',
+      selector: (row) => row.age,
+      sortable: true,
+    },
+    {
+      name: 'Sex',
+      selector: (row) => row.sex,
+      sortable: true,
+      right: true,
+    },
+    {
+      name: 'Treatment Costs',
+      selector: (row) => row.treatmentCosts,
+      format: (row) => row.treatmentCosts.toLocaleString('en-US', {style: 'currency', currency: 'USD'}),
+      sortable: true,
+      right: true,
+    },
+  ]), []) // eslint-disable-line react-hooks/exhaustive-deps
 
   if (loading) return <Loading />
 
