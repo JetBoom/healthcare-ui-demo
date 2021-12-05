085d806718e190e84465bdc0b5a72495009af2c7
William Moodhe <williammoodhe@gmail.com>
2021-12-05 16:30:03 -0500

    Make a simple data table for patients list

--- src/routes/patients/PatientsListPage.js
+++ src/routes/patients/PatientsListPage.js
@@ -1,10 +1,71 @@
+import { useState, useEffect } from 'react'
+import DataTable from 'react-data-table-component'
 import Page from 'components/Page'
+import Loading from 'components/Loading'
+import { GET } from 'util/dev'
 
+
+const columns = [
+  {
+    name: 'Name',
+    selector: (row) => row.name,
+    sortable: true,
+    grow: 2,
+  },
+  {
+    name: 'Age',
+    selector: (row) => row.age,
+    sortable: true,
+  },
+  {
+    name: 'Sex',
+    selector: (row) => row.sex,
+    sortable: true,
+    right: true,
+  },
+  {
+    name: 'Treatment Costs',
+    selector: (row) => row.treatmentCosts,
+    format: (row) => row.treatmentCosts.toLocaleString('en-US', {style: 'currency', currency: 'USD'}),
+    sortable: true,
+    right: true,
+  },
+]
 
 export default function PatientsListPage() {
+  const [loading, setLoading] = useState(false)
+  const [patients, setPatients] = useState([])
+
+  useEffect(() => {
+    // useEffect can't use an async function so we call it this way.
+    const fetchData = async () => {
+      setLoading(true)
+
+      try {
+        const patientsData = await GET('/testdata/patients.json', 'json')
+
+        setPatients(patientsData)
+
+        setLoading(false)
+      } catch (e) {
+        console.error(`Error fetching data: ${e}`)
+      }
+    }
+
+    fetchData()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (loading) return <Loading />
+
   return (
     <Page>
-      Patients
+      <div style={{width: '100%'}}>
+        <DataTable
+          title="All Patients"
+          data={patients}
+          columns={columns}
+        />
+      </div>
     </Page>
   )
 }
