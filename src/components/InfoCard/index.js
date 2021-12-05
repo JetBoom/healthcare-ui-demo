905fffe175abfebfb2228ba843e1cedae534b903
William Moodhe <williammoodhe@gmail.com>
2021-12-04 21:47:11 -0500

    InfoCard to single directory with central importer

---
+++ src/components/InfoCard/index.js
@@ -0,0 +1,13 @@
+import InfoCard from './InfoCard'
+import InfoCardHeader from './InfoCardHeader'
+import InfoCardTitle from './InfoCardTitle'
+import InfoCardGrid from './InfoCardGrid'
+
+
+export default InfoCard
+export {
+  InfoCard,
+  InfoCardHeader,
+  InfoCardTitle,
+  InfoCardGrid,
+}
