a97a83a4989df9038e672fa8d75460fc57e7455c
William Moodhe <williammoodhe@gmail.com>
2021-12-05 19:53:24 -0500

    DoctorInfo tests

--- src/components/DoctorInfo/DoctorInfo.js
+++ src/components/DoctorInfo/DoctorInfo.js
@@ -16,15 +16,19 @@ DoctorInfo.propTypes = {
 
 function DoctorInfo({ classes, doctor }) {
   return (
-    <div className={classes.root}>
+    <div className={classes.root} data-testid="doctorinfo">
       <div className={classes.info}>
-        <Avatar src={doctor.avatarUrl} className={classes.avatar} />
+        <Avatar src={doctor.avatarUrl} className={classes.avatar} data-testid="avatar" />
         <div>
-          <Typography variant="h5" className={classes.nameInfo}>{doctor.name}, {doctor.title}</Typography>
-          <Typography className={classes.subInfo}>{doctor.department}</Typography>
+          <Typography variant="h5" className={classes.nameInfo} data-testid="nametitle">
+            {doctor.name}, {doctor.title}
+          </Typography>
+          <Typography className={classes.subInfo} data-testid="department">
+            {doctor.department}
+          </Typography>
         </div>
       </div>
-      <Typography variant="body1" className={classes.bio}>
+      <Typography variant="body1" className={classes.bio} data-testid="bio">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Typography>
     </div>
---
+++ src/components/DoctorInfo/DoctorInfo.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import DoctorInfo from './DoctorInfo';
+
+
+const doctor = {
+  name: 'John Smith',
+  title: 'MD',
+  department: 'Surgery',
+  avatarUrl: '/testcontent/pfp1.jpg',
+}
+
+describe('<DoctorInfo>', () => {
+  test('it renders', () => {
+    expect(
+      () => render(<DoctorInfo doctor={doctor} />)
+    ).not.toThrow()
+
+    expect(screen.getByTestId('doctorinfo')).toBeVisible()
+
+    expect(screen.getByTestId('nametitle')).toHaveTextContent(`${doctor.name}, ${doctor.title}`)
+
+    expect(screen.getByTestId('department')).toHaveTextContent(doctor.department)
+
+    expect(screen.getByTestId('avatar')).toBeVisible()
+
+    expect(screen.getByTestId('bio')).toBeVisible()
+  })
+})
